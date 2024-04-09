7568a62bc3b5f2726bfc8c9ac063f31a36006268
Dustin Ruck <dustin.ruck@gmail.com>
2024-04-09 01:50:13 -0400

    fixing a bug

--- src/main/java/com/example/group4/sportsclub/SportsClubApplication.java
+++ src/main/java/com/example/group4/sportsclub/SportsClubApplication.java
@@ -3,10 +3,10 @@ package com.example.group4.sportsclub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.filter.CorsFilter;
 
 
 @SpringBootApplication
