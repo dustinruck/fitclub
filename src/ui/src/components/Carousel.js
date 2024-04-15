7ba91313f4a6e45790fed290801458c3fd9d0ad0
Akashdeep Singh Khinda <134426272+StylishKash@users.noreply.github.com>
2024-04-15 13:53:14 -0400

    Adding new components

---
+++ src/ui/src/components/Carousel.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image1 from "../pictures/dance.jpg";
+import image2 from "../pictures/dance.jpg";
+import image3 from "../pictures/dance.jpg";
+import './Carousel.css';
+
+const MyCarousel = () => {
+  return (
+    <Carousel>
+      <Carousel.Item style={{ height: '85vh' }}>
+        <img
+          className="d-block w-100"
+          src={image1}
+          alt="First slide"
+          style={{ height: '100%', objectFit: 'cover' }}
+        />
+        <Carousel.Caption>
+          <h3 className='margin-middle'>OFFERING OVER 50 CLASSES PER WEEK, ZEN IS THE ONLY STUDIO IN MONTREAL WITH THE GREATEST DIVERSITY OF DANCE, YOGA AND FITNESS CLASSES, ALL IN ONE PLACE!</h3>
+          <button className='btn1 margin-middle'>EXPLORE CLASSES</button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item style={{ height: '85vh' }}>
+        <img
+          className="d-block w-100"
+          src={image2}
+          alt="Second slide"
+          style={{ height: '100%', objectFit: 'cover' }}
+        />
+        <Carousel.Caption>
+          <h3 className='margin-middle'>WITH VARIOUS CLASSES OFFERED EVERYDAY, YOU ARE BOUND TO FIND SOMETHING MADE FOR YOUR SCHEDUELE. CHECK OUT OUR CALENDAR TO SEE THE LATEST!</h3>
+          <button className='btn1 margin-middle'>CALENDAR</button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item style={{ height: '85vh' }}>
+        <img
+          className="d-block w-100"
+          src={image3}
+          alt="Third slide"
+          style={{ height: '100%', objectFit: 'cover' }}
+        />
+        <Carousel.Caption>
+          <h3 className='margin-middle'>KEEP UP WITH UPCOMING EVENTS AND SPECIALS BY CHECKING UP ON OUR BLOG FOR WEEKLY UPDAPTES!</h3>
+          <button className='btn1 margin-middle'>BLOG</button>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default MyCarousel;
--- src/ui/src/components/Footer.js
+++ src/ui/src/components/Footer.js
@@ -1,14 +1,35 @@
 // Footer //
 
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
 
-function Footer() {
+const Footer = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand>Footer</Navbar.Brand>
-        </Navbar>
+      <footer className="myfooter py-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+            <h5>Reach Us</h5>
+            <ul className="list-unstyled">
+                <li>Address: 551 Avenue du Mont Royal Est, 3e Etage</li>
+                <li>Montreal, Quebec</li>
+            </ul>
+            </div>
+            <div className="col-md-6">
+              <h5>Contact Us</h5>
+              <ul className="list-unstyled">
+                <li>Email: example@example.com</li>
+                <li>Phone: 123-456-7890</li>
+              </ul>
+            </div>
+            <div className="col-md-3">
+            <ul className="list-unstyled">
+                <div className="footer-copyright text-right py-3">© 2020 Copyright: ZEN</div>
+            </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     );
-}
+  };
 
 export default Footer;
