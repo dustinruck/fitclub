cbb91a03fe976076b1be2b0bcbac19f629edcddd
Akashdeep Singh Khinda <134426272+StylishKash@users.noreply.github.com>
2024-04-15 13:53:14 -0400

    Adding new components

--- src/ui/src/components/Header.js
+++ src/ui/src/components/Header.js
@@ -4,30 +4,26 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import LoginButton from './LoginButton';
+import './Header.css';
 
 function Header() {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+        <Navbar expand="lg" fixed="top">
+            <Navbar.Brand as={Link} to="/" className="logo">ZEN</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link as={Link} to="/about">About</Nav.Link>
-                    <Nav.Link as={Link} to="/classes">Classes</Nav.Link>
-                    <Nav.Link as={Link} to="/calendar">Calendar</Nav.Link>
-                    <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-                    <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
-                    <Nav.Link as={Link} to="/admin">Admin</Nav.Link>
-
-                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                    <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
-                    <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+                <Nav className="mx-auto">
+                    <Nav.Link as={Link} to="/about">ABOUT</Nav.Link>
+                    <Nav.Link as={Link} to="/classes">CLASSES</Nav.Link>
+                    <Nav.Link as={Link} to="/calendar">CALENDAR</Nav.Link>
+                    <Nav.Link as={Link} to="/blog">BLOG</Nav.Link>
+                    <Nav.Link as={Link} to="/profile">PROFILE</Nav.Link>
+                    <Nav.Link as={Link} to="/admin">ADMIN</Nav.Link>
                 </Nav>
-                {/* <LoginButton /> */}
+                <LoginButton />
             </Navbar.Collapse>
         </Navbar>
     );
 }
 
 export default Header;
-
