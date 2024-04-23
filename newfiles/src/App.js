ec4237d375cd679ca9e11eae379f95f546b670cc
Dustin Ruck <dustin.ruck@gmail.com>
2024-04-23 11:39:05 -0400

    merge

---
+++ newfiles/src/App.js
@@ -0,0 +1,86 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
+import DataFetcher from "./API/DataFetcher";
+import Add from "./component/Add";
+import { Button } from "bootstrap";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+//import todoList from './component/todo-list';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <div className="nav nav-tabs nav-pills bg-dark" id="nav-tab" role="tablist">
+          <button
+            className="nav-link active"
+            id="nav-home-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#nav-home"
+            type="button"
+            role="tab"
+            aria-controls="nav-home"
+            aria-selected="true"
+          >
+            Classes
+          </button>
+          <button
+            className="nav-link"
+            id="nav-profile-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#nav-profile"
+            type="button"
+            role="tab"
+            aria-controls="nav-profile"
+            aria-selected="false"
+          >
+            Trainers
+          </button>
+          <button
+            className="nav-link"
+            id="nav-contact-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#nav-contact"
+            type="button"
+            role="tab"
+            aria-controls="nav-contact"
+            aria-selected="false"
+          >
+            Contact
+          </button>
+        </div>
+      </nav>
+      <div className="tab-content" id="nav-tabContent">
+        <div
+          className="tab-pane fade show active"
+          id="nav-home"
+          role="tabpanel"
+          aria-labelledby="nav-home-tab"
+        >
+          classes table
+        </div>
+        <div
+          className="tab-pane fade"
+          id="nav-profile"
+          role="tabpanel"
+          aria-labelledby="nav-profile-tab"
+        >
+          <Add />
+          <DataFetcher />
+        </div>
+        <div
+          className="tab-pane fade"
+          id="nav-contact"
+          role="tabpanel"
+          aria-labelledby="nav-contact-tab"
+        >
+          ...
+        </div>
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
