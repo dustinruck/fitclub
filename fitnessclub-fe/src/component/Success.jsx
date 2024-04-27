4ab3accdb11765b50c9e133dc6d7f003f7882fd6
WissamEng <66325125+WissamEng@users.noreply.github.com>
2024-04-27 08:16:21 -0400

    Add files via upload
    fix payment

--- fitnessclub-fe/src/component/Success.jsx
+++ fitnessclub-fe/src/component/Success.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+/* SuccessPage.js */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/pay.css'; 
 
 const Success = () => {
   return (
-    <div>Success</div>
-  )
-}
+    <div className="success-page">
+      <h2>Payment Successful!</h2>
+      <p>Thank you for your purchase.</p>
+      <Link to="/">Return to Home</Link>
+    </div>
+  );
+};
 
-export default Success
+export default Success;
